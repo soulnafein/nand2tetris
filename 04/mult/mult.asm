0138ce0c31a96d0fb630b05929fce1fcc6959c43
David Santoro <soulnafein@gmail.com>
2018-10-05 09:10:41 +0100

    Mult.asm works

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Set R2 to 0
+@2
+D = M
+@0
+D = A
+@2
+M = D
+
+
+(LOOP)
+// Finish if R1 is 0
+@1
+D = M
+@END
+D; JEQ
+
+// Adds R0 to R2
+@2
+D = M
+@0
+D = D + M
+@2
+M = D
+
+// Descrement R1
+@1
+M = M - 1
+
+// Loops
+@LOOP
+0; JMP
+
+(END)
